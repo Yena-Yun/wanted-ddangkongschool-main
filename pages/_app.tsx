2894c6405595253b8e3db924acebc4531cabb4c1
minjuice1 <minjuice93@gmail.com>
2022-02-14 23:34:07 +0900

    :lipstick: UI 컴포넌트 5번의 png 추가, 대략적인 레이아웃 구성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import { AppProps } from 'next/app';
+import Style5 from 'src/component/Style5';
+import '../styles/globals.css'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return(
+    <>
+    <Component {...pageProps} />
+    <Style5/>
+    </>
+  )
 }
 
 export default App;
